a2abe3dd27caba7edd40cb294f15ae8797242250
aprilnicole013 <aprilnicole013@gmail.com>
2022-01-25 21:28:03 -0500

    Create higher order function with showMovies()

--- script.js
+++ script.js
@@ -2,33 +2,43 @@ const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280'
 
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='
+
 const main = document.querySelector('main')
+const form = document.querySelector('form')
+const search = document.querySelector('search')
+
+getMovies()
 
 async function getMovies(){
     const resp = await fetch(APIURL)
     const respData = await resp.json()
 
-    console.log(respData)
 
-    respData.results.forEach(movie => {
-    const { poster_path, title, vote_average } = movie;
+    showMovies(respData.results)
+}
 
-        const movieEl = document.createElement('div')
+function showMovies(movies){
 
-        movieEl.classList.add('movie')
-        
-        movieEl.innerHTML = `
-            <img src='${IMGPATH + poster_path}' alt='${title}'/>
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-            </div>
-        `
+    main.innerHTML = ''
 
-        main.appendChild(movieEl)
-    })
+    movies.forEach(movie => {
+        const { poster_path, title, vote_average } = movie;
     
-    return respData
+            const movieEl = document.createElement('div')
+    
+            movieEl.classList.add('movie')
+            
+            movieEl.innerHTML = `
+                <img src='${IMGPATH + poster_path}' alt='${title}'/>
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                </div>
+            `
+    
+            main.appendChild(movieEl)
+        })
 }
 
 function getClassByRate(vote){
@@ -41,4 +51,8 @@ function getClassByRate(vote){
     }
 }
 
-getMovies()
+form.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+})
